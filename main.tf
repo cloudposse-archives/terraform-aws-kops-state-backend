c60cb982e30dadb8baed386498d19a1d7b35f8bd
Ciaran Kelly <ciaran.kelly@gmail.com>
2020-04-14 12:37:39 +0100

    Terraform 0.12 changes
    remove some quoted strings
    
    change route53 cluster zone version
    
    fix resource indexing
    
    fix resource indexing
    
    fix resource indexing
    
    fix null_resource version
    
    fix variable types

--- main.tf
+++ main.tf
@@ -2,73 +2,71 @@ provider "aws" {
   version = "~> 2.17"
 
   alias  = "s3"
-  region = "${var.region}"
+  region = var.region
 }
 
 data "template_file" "zone_name" {
-  template = "${replace(var.zone_name, "$$$$", "$")}"
+  template = replace(var.zone_name, "$$$$", "$")
 
-  vars {
-    namespace        = "${var.namespace}"
-    name             = "${var.cluster_name}"
-    stage            = "${var.stage}"
-    parent_zone_name = "${var.parent_zone_name}"
+  vars = {
+    namespace        = var.namespace
+    name             = var.cluster_name
+    stage            = var.stage
+    parent_zone_name = var.parent_zone_name
   }
 }
 
 locals {
-  create_s3_bucket = "${!(var.create_bucket == "false")}"
+  create_s3_bucket = !(var.create_bucket == "false")
 
-  tags = "${
-      merge(
+  tags = merge(
         var.tags,
         map(
-          "Cluster", "${data.template_file.zone_name.rendered}"
+          "Cluster", data.template_file.zone_name.rendered
         )
       )
-    }"
 }
 
 # Kops domain (e.g. `kops.domain.com`)
 module "domain" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git?ref=tags/0.3.1"
-  namespace        = "${var.namespace}"
-  name             = "${var.cluster_name}"
-  stage            = "${var.stage}"
-  delimiter        = "${var.delimiter}"
-  attributes       = "${var.attributes}"
-  zone_name        = "${var.zone_name}"
-  parent_zone_id   = "${var.parent_zone_id}"
-  parent_zone_name = "${var.parent_zone_name}"
-  tags             = "${local.tags}"
-  enabled          = "${var.domain_enabled}"
+  source           = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git?ref=tags/0.4.0"
+  namespace        = var.namespace
+  name             = var.cluster_name
+  stage            = var.stage
+  delimiter        = var.delimiter
+  attributes       = var.attributes
+  zone_name        = var.zone_name
+  parent_zone_id   = var.parent_zone_id
+  parent_zone_name = var.parent_zone_name
+  tags             = local.tags
+  enabled          = var.domain_enabled
 }
 
 module "s3_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${local.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = local.tags
 }
 
 data "aws_s3_bucket" "default" {
-  provider = "aws.s3"
+  provider = aws.s3
 
-  count  = "${local.create_s3_bucket ? 0 : 1}"
-  bucket = "${module.s3_label.id}"
+  count  = local.create_s3_bucket ? 0 : 1
+  bucket = module.s3_label.id
 }
 
 resource "aws_s3_bucket" "default" {
-  provider = "aws.s3"
+  provider = aws.s3
 
-  count         = "${local.create_s3_bucket ? 1 : 0}"
-  bucket        = "${module.s3_label.id}"
-  acl           = "${var.acl}"
-  region        = "${var.region}"
-  force_destroy = "${var.force_destroy}"
+  count         = local.create_s3_bucket ? 1 : 0
+  bucket        = module.s3_label.id
+  acl           = var.acl
+  region        = var.region
+  force_destroy = var.force_destroy
 
   versioning {
     enabled = true
@@ -82,14 +80,14 @@ resource "aws_s3_bucket" "default" {
     }
   }
 
-  tags = "${module.s3_label.tags}"
+  tags = module.s3_label.tags
 }
 
 resource "aws_s3_bucket_public_access_block" "default" {
-  provider = "aws.s3"
+  provider = aws.s3
 
-  count  = "${local.create_s3_bucket && var.block_public_access_enabled == "true" ? 1 : 0}"
-  bucket = "${aws_s3_bucket.default.id}"
+  count  = local.create_s3_bucket && var.block_public_access_enabled == "true" ? 1 : 0
+  bucket = aws_s3_bucket.default[0].id
 
   block_public_acls       = true
   block_public_policy     = true
